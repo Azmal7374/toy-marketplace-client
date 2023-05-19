db82b5445838e65dca431b040bdab911c8fbf6b4
Azmal <azmalgazi8@gmail.com>
2023-05-19 18:41:22 +0600

    view datailes page updated

--- src/pages/toydDetails/ToyDetails.jsx
+++ src/pages/toydDetails/ToyDetails.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Rating } from '@smastrom/react-rating';
 import '@smastrom/react-rating/style.css';
+import { AuthContext } from '../../provider/AuthProvider';
+import Sppiner from '../share/Spinner/Sppiner';
 
 const ToyDetails = () => {
+    const{loading} =useContext(AuthContext)
     const toys =useLoaderData()
-    const {_id, name, price,picture,ratting} =toys;
+    const {_id, name, price,picture,ratting, quatntiy, description ,sellerName,sellerEamil} =toys;
+    console.log(toys);
+
+    if(loading){
+     return <Sppiner></Sppiner>
+    }
+
     return (
         <div className='mt-10 p-8'>
             <div className="md:flex">
             <div>
-            <img className="" src={picture} alt="" />
+            <img className="w-4/5 md:w-auto" src={picture} alt="" />
             </div>
-            <div className='bg-slate-200 w-4/5 p-8'>
-            <p className="text-2xl"><span className=" font-bold">{name}</span></p>
+            <div className='bg-slate-200 w-4/5 h-content p-8'>
+            <p className="text-2xl">Seller Name: <span className=" font-bold">{sellerName}</span></p> 
+              
+            <p className="text-2xl">Seller Email: <span className=" font-bold">{sellerEamil}</span></p>
+            <p className="text-2xl">Toy Name: <span className=" font-bold">{name}</span></p>
+            
             <p className="text-xl">Price: <span className=" font-semibold">${price}</span></p>
            <div className="flex">
            <p className="text-xl">Ratting: </p>
@@ -23,7 +36,8 @@ const ToyDetails = () => {
          readOnly
          ></Rating>
            </div>
-            <p className="text-3xl"><span className=" font-bold">{}</span></p>
+            <p className="text-xl">Availabe Quantity: <span className=" font-semibold">{quatntiy}</span></p>
+            <p className="text-xl">Details Description: <span className=" font-bold">{description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi repellendus quia unde ullam vitae animi eum autem numquam aperiam in? Enim ipsum repudiandae necessitatibus laborum soluta esse, recusandae quasi neque voluptates, asperiores debitis incidunt, earum nobis adipisci ipsa quod repellat!</span></p>
             </div>
             </div>
         </div>
